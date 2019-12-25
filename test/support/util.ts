de7596a669ed13c00b97ddb031eaaa6f006772fe
SimaQ <sima.zhang1990@gmail.com>
2019-12-25 14:12:02 +0800

    Revert "V0.11"

--- test/support/util.ts
+++
@@ -1,6 +0,0 @@
-import { readFileSync } from 'fs';
-import { resolve } from 'path';
-
-export default (pathname: string): string => {
-  return readFileSync(resolve(process.cwd(), pathname), 'utf8');
-};
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,44 @@
-//
+const webpack = require('webpack');
 const resolve = require('path').resolve;
 const pkg = require('./package.json');
 
 module.exports = {
   devtool: 'cheap-source-map',
-  mode: 'development',
   entry: {
-    'data-set': './src/index.ts',
-  },
-  resolve: {
-    extensions: ['.js', '.ts', '.json'],
+    'data-set': './src/index.js'
   },
   output: {
     filename: '[name].js',
     library: 'DataSet',
     libraryTarget: 'umd',
-    path: resolve(__dirname, 'build/'),
+    path: resolve(__dirname, 'build/')
   },
   module: {
     rules: [
       {
-        test: /\.ts$/,
+        test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'awesome-typescript-loader',
+        use: {
+          loader: 'babel-loader',
+          options: {
+            babelrc: true
+          }
+        }
       },
       {
         test: /data\-set\.js$/,
-        loader: 'string-replace-loader',
-        options: {
-          search: '____DATASET_VERSION____',
-          replace: pkg.version,
-        },
-      },
-    ],
+        use: {
+          loader: 'string-replace-loader',
+          options: {
+            search: '____DATASET_VERSION____',
+            replace: pkg.version
+          }
+        }
+      }
+    ]
   },
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.optimize.AggressiveMergingPlugin()
+  ]
 };
